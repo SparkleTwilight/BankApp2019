7edd033d709390efcdb7454dbd20a76d7c0ff5c2
hackworth.ameliah@gmail.com <hackworth.ameliah@gmail.com>
2019-05-23 14:41:19 -0500

    Recreated Users model file

--- api/models/Users.js
+++ api/models/Users.js
@@ -4,21 +4,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let Users = new Schema({
-
-    username: {
-        type: String
-    },
-    password: {
-        type: String
-    },
-    first_name: {
-        type: String
-    },
-    last_name: {
-        type: String
-    }
-
-
+ username: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  first_name: {
+    type: String
+  },
+  last_name: {
+    type: String
+  }
 },{
     collection: 'bankApp'
 });
