f9988170fa66a0ffe67944dfbd7945797a7f472d
hackworth.ameliah@gmail.com <hackworth.ameliah@gmail.com>
2019-06-17 03:40:14 -0500

    Changed server port number from 4000 to 3000

--- api/server.js
+++ api/server.js
@@ -18,8 +18,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/users', usersRoute);
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, function(){
-    console.log('Listening on port' + port);
+    console.log('Listening on port ' + port);
 });
